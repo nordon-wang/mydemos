062861cb91cfd255779694e9e5b5eb3fa0ede079
nordon-wang <1531959404@qq.com>
2018-03-14 23:34:08 +0800

    node fs

--- node/api-fs.js
+++ node/api-fs.js
@@ -1,10 +1,84 @@
 /**
  * fs
+ *  同步的错误才可以使用 try catch捕获异常
  */
 
 const fs = require('fs');
 const path = require('path');
 
+// require('./proto.js');
 
+/**
+ *  意外错误(磁盘空间不足等...)
+ *  文件权限问题
+ *  文件夹找不到
+ */
 
-console.log(fs);
+// 文件写入
+// let filename = path.join(__dirname, 'www/b.txt');
+// fs.writeFile(path.join(__dirname,'www/b.txt'),JSON.stringify({"ID":123}),(err) => {
+//     console.log(err)
+// });
+
+// fs.writeFileSync(path.join(__dirname,'www/b.txt'),JSON.stringify({"ID":123}));
+
+// let streamWrite = fs.createWriteStream(path.join(__dirname,'www/b.txt'));
+// setInterval(() => {
+//     streamWrite.write('hello streamWrite',err => {
+//         console.log(`+1`);
+//     });
+// },500);
+
+// fs.appendFile(filename,'appendFile', err =>{
+// })
+
+// fs.appendFileSync(filename,'appendFileSync');
+
+// 打印当前目录下所有文件
+
+// fs.readdir(__dirname,(err,files) => {
+//     files.forEach( file => {
+//         // console.log(__dirname,file);
+//         fs.stat(path.join(__dirname,files[0]),(err,stats) => {
+//             console.log(`${stats.size}--${stats.mtime.format('yyyy-MM-dd HH:mm:s')}--${file}`);
+//         })
+//     });
+// })
+
+// 递归打印目录树
+
+let target = path.join(__dirname, process.argv[2] || './');
+
+function load(target, depth) {
+
+    let prefix = new Array(depth + 1).join('│ ');
+
+    let dirinfos = fs.readdirSync(target);
+
+    let dirs = [];
+    let files = [];
+
+    dirinfos.forEach(info => {
+        let stats = fs.statSync(path.join(target, info));
+
+        if (stats.isFile()) {
+            files.push(info);
+        } else {
+            dirs.push(info);
+        }
+    });
+
+    dirs.forEach(dir => {
+        console.log(`${prefix}├─${dir}`);
+        load(path.join(target, dir), depth + 1);
+    });
+
+    var count = files.length - 1;
+    files.forEach(file => {
+        var temp = count-- ? '├' : '└';
+        console.log(`${prefix}${temp}─${file}`);
+    });
+
+}
+
+load(target, 0);
--- node/lrc.js
+++ node/lrc.js
@@ -1,50 +1,84 @@
 /**
  * 歌词显示
  *  由于node不支持bgk的转码
- *  使用iconv lite 扩展包进行转码
+ *  使用iconv lite 扩展包进行转码 -- 转buffer
  */
-const fs  = require('fs');
+const fs = require('fs');
 const path = require('path');
 const iconv = require('iconv-lite');
 
-fs.readFile(path.join(__dirname,'lrc/传奇.lrc'),(err,data) => {
-    // console.log(iconv.decode(data,'gbk'));
-    let lrcs = iconv.decode(data,'gbk');
+// 一行一行读
+const readline = require('readline');
 
-    // 换行 可能是 \r\n 或者 \n
-    let myLrcs = lrcs.split('\n');
-    // [00:23.27]短暂的狂欢以为一生绵延
-    let reg = /\[(\d{2})\:(\d{2})\.(\d{2})\](.+)/;
+// [00:23.27]短暂的狂欢以为一生绵延
+let reg = /\[(\d{2})\:(\d{2})\.(\d{2})\](.+)/;
 
-    let beginTime = Date.now();
+function readLrc(item, beginTime) {
+    let matches = reg.exec(item);
+    // console.log(matches)
+    // 歌词主体判断
+    if (matches) {
+        // 获取 分 秒 毫秒
+        let m = parseInt(matches[1]);
+        let s = parseInt(matches[2]);
+        let ms = parseInt(matches[3]);
+        let lyric = matches[4];
 
-    myLrcs.forEach((item,index) => {
-        let matches = reg.exec(item);
-        // console.log(matches)
-        // 歌词主体判断
-        if(matches){
-            // 获取 分 秒 毫秒
-            let m = parseInt(matches[1]);
-            let s = parseInt(matches[2]);
-            let ms = parseInt(matches[3]);
-            let lyric = matches[4];
+        // 当前行歌词 需要在特定时间显示
+        // 由于下达输出的任务的时刻不同 可能会导致误差
+        // 在定时器中 将误差的时间去除 就是正常时间
+        let currentTime = Date.now() - beginTime;
+        setTimeout(() => {
+            console.log(lyric)
+        }, m * 60 * 1000 + s * 1000 + ms - currentTime);
+    } else {
+        console.log(item)
+    }
+}
 
-            // 当前行歌词 需要在特定时间显示
-            // 由于下达输出的任务的时刻不同 可能会导致误差
-            // 在定时器中 将误差的时间去除 就是正常时间
-            let currentTime = Date.now() - beginTime;
-            setTimeout(()=>{
-                console.log(lyric)
-            }, m * 60 * 1000 + s * 1000 + ms - currentTime);
-        }else{
-            console.log(item)
-        }
+// fs.readFile(path.join(__dirname, 'lrc/传奇.lrc'), (err, data) => {
+//     // console.log(iconv.decode(data,'gbk'));
+//     let lrcs = iconv.decode(data, 'gbk');
+//     // 换行 可能是 \r\n 或者 \n
+//     let myLrcs = lrcs.split('\n');
 
-    });
+//     let beginTime = Date.now();
 
-    console.log(myLrcs.length);
+//     myLrcs.forEach((item, index) => {
+//         readLrc(item, beginTime);
+//     });
+
+// });
+
+// 通过readline读取歌词
+let filename = path.join(__dirname,'lrc/传奇.lrc');
+// -- 利用iconv-lite进行转码
+let streamRead = fs.createReadStream(filename)
+    .pipe(iconv.decodeStream('gbk'));
+
+// 利用readline读取 
+let rl = readline.createInterface({
+    input:streamRead
+});
 
 
+let beginTime2 = Date.now();
+rl.on('line', item => {
+    readLrc(item,beginTime2)
+});
+
+
+// 流的读取方式
+let mydatas = '';
+streamRead.on('data',chunk => {
+    // chunk只是文档的一个不完整的片段
+    // mydatas += chunk.toString();
+    mydatas += iconv.decode(chunk,'gbk');
+});
+
+streamRead.on('end',() =>{
+    // 文档读取结束 此时的mydatas是完整的
+    // console.log(mydatas);
 });
 
 
